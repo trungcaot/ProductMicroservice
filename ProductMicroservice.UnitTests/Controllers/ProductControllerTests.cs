0fb908de44ee1c9535681d37608e34dcd776016d
Cao Thanh Trung <caothanhtrung4193@gmail.com>
2019-10-12 22:51:41 +0700

    Implemented Unit Test with xUnit and Fluent API.

---
+++ ProductMicroservice.UnitTests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using ProductMicroservice.Controllers;
+using ProductMicroservice.UnitTests.MockRepositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ProductMicroservice.UnitTests.MockRepositories;
+using Moq;
+
+namespace ProductMicroservice.UnitTests.Controllers
+{
+    public class ProductControllerTests
+    {
+        private readonly MockProductRepository mockProductRepository;
+        private ProductController _productController;
+
+        public ProductControllerTests()
+        {
+            mockProductRepository = new MockProductRepository();
+
+            _productController = new ProductController(mockProductRepository.Object);
+        }
+
+        [Fact]
+        public void GetProduct_WithProductIsNotNull_Success()
+        {
+            // Arrange 
+            mockProductRepository.MockGetProducts();
+
+            // Act
+            var result = _productController.Get();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            mockProductRepository.VerifyGetProducts(Times.Once());
+        }
+    }
+}
---
+++ ProductMicroservice.UnitTests/MockRepositories/MockProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using ProductMicroservice.Models;
+using ProductMicroservice.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+
+namespace ProductMicroservice.UnitTests.MockRepositories
+{
+    public class MockProductRepository : Mock<IProductRepository>
+    {
+        public MockProductRepository MockGetProducts()
+        {
+            Setup(p => p.GetProducts())
+                .Returns(new List<Product>()).Verifiable();
+
+            return this;
+        }
+
+        public MockProductRepository VerifyGetProducts(Times times)
+        {
+            Verify(p => p.GetProducts(), times);
+
+            return this;
+        }
+    }
+}
